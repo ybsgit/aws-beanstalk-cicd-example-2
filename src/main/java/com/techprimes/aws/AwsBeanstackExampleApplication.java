7f58df4c9f201a8140eb1e0230d46e58b7578ef4
Your Name <you@example.com>
2021-11-16 10:11:17 +0530

    first commit

---
+++ src/main/java/com/techprimes/aws/AwsBeanstackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimes.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstackExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techprimes/aws/AwsBeanstackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimes.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
